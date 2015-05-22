87beff7786ab4f9446adbfffbdd0a0a2a5689334
surreptakos <djgatsakos@gmail.com>
2015-05-21 19:18:27 -0500

    Handling Exceptions Tutorial
    from caveofprogramming.com's Java for Complete Beginners course

---
+++ demo2/App.java
@@ -0,0 +1,27 @@
+package demo2;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		File file = new File("test.txt");
+		
+	
+		try {
+			FileReader fr = new FileReader(file);
+			
+			//This next line is not run if an exception is thrown
+			System.out.println("Continuing...");
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found: " + file.toString());
+		}
+		
+		System.out.println("Finished.");
+	}
+
+}
---
+++ demo3/App.java
@@ -0,0 +1,26 @@
+package demo3;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+
+public class App {
+
+	public static void main(String[] args) {
+		//Never show the user the stack trace!
+		try {
+			openFile();
+		} catch (FileNotFoundException e) {
+			//This message is too vague
+			System.out.println("Could not open file.");
+		}
+	}
+	
+	public static void openFile() throws FileNotFoundException {
+		File file = new File("test.txt");
+		
+		FileReader fr = new FileReader(file);
+		
+	}
+
+}
